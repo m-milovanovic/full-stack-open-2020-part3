62cca8ac7d6e3294209a047cfa8761372df47162
m-milovanovic <m.milovanovic@outlook.com>
2020-09-26 18:35:31 +0200

    uibuild

---
+++ build/precache-manifest.432f99d003f5be5989f9d273686ceef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544341cea17ad5f32d090f8fe4f095c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9317754f29f0e81ffce",
+    "url": "/static/css/main.a34e5709.chunk.css"
+  },
+  {
+    "revision": "23ae8b4bdb7638792266",
+    "url": "/static/js/2.021188dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.021188dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9317754f29f0e81ffce",
+    "url": "/static/js/main.751ecaee.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
